76c55fdcb0b580feb16d78dc75df5f81f19e14c6
32vusie <77724400+32vusie@users.noreply.github.com>
2021-02-16 14:07:46 +0200

    added on backpressed exit with dialog

--- lib/helpers/opertations.dart
+++ lib/helpers/opertations.dart
@@ -1,9 +1,21 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:system32online_portal/webview_container/webview_container.dart';
 
-_handleURLButtonPress(BuildContext context, String url) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => WebViewContainer(url)));
+Future<bool> onWillPop() async {
+    return (await showDialog(
+      // context: context,
+      builder: (context) => new AlertDialog(
+        title: new Text('Are you sure?'),
+        content: new Text('Do you want to exit an App'),
+        actions: <Widget>[
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('No'),
+          ),
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Yes'),
+          ),
+        ],
+      ), context: null,
+    )) ?? false;
 }
